73e083a7348d4cad1831f578a1dbe70a13860935
sohta <sohta02@fa2.so-net.ne.jp>
2018-07-29 00:00:00 +0900

    2018/07/29 ファミリーデー配布バージョン

---
+++ burn.bat
@@ -0,0 +1,2 @@
+avrdude -Cavrdude.conf -v -patmega328p -cstk500v1 -PCOM10 -b19200 -e -Ulock:w:0x3F:m -Uefuse:w:0xFD:m -Uhfuse:w:0xDE:m -Ulfuse:w:0xFF:m 
+avrdude -Cavrdude.conf -v -patmega328p -cstk500v1 -PCOM10 -b19200 -Uflash:w:mBot-master.2018FD.ino.with_bootloader.standard.hex:i -Ulock:w:0x0F:m 
